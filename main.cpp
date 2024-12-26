b7a259328694471cc2fadac6c0c41a5b9955a20b
Romelianism <Romelianism@gmail.com>
2024-12-27 05:02:10 +0800

    feat: :sparkles: add init code to bodies
    allow bodies to be modified and set to_string function appropriately const

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ struct Body {
   double x, y, z;
   double mass;
 
-  std::string to_string() {
+  std::string to_string() const {
     return std::format("position: {}, {}, {} | mass: {}", x, y, z, mass);
   }
 };
@@ -17,7 +17,15 @@ typedef unsigned int uint;
 
 int main() {
   const uint number_of_bodies = 2;
-  const std::array<Body, number_of_bodies> bodies{};
+  std::array<Body, number_of_bodies> bodies{};
+
+  // Init bodies
+  for (Body &body : bodies) {
+    body.x = 0;
+    body.y = 0;
+    body.z = 0;
+    body.mass = 1;
+  }
 
   // Print to console
   for (Body const &body : bodies) {
